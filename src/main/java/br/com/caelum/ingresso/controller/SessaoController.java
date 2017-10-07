2d2d1c0e3d32ed15bf7a79a23eb48a74019647f9
Mario Grejo <mgrejo@gmail.com>
2017-10-07 17:01:41 -0300

    aula de 07 out

---
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -0,0 +1,20 @@
+package br.com.caelum.ingresso.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SessaoController {
+
+	@GetMapping("/admin/sessao")
+	public ModelAndView form(@RequestParam("salaId") Integer salaId) {
+		
+		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+		
+		return modelAndView;
+		
+	}
+	
+}
